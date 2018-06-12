592977f23699c8cd32f5aa8a6b6c20383099ba73
Addis Casco <addis.casco@utexas.edu>
2018-06-12 18:34:18 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 //set up port
 const PORT = process.env.PORT || 3000;
@@ -20,11 +20,11 @@ require("./config/routes")(router);
 app.use(express.static(__dirname + "/public"));
 
 //connect handlebars to express app
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //use bodyparser in app
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //every request goes through our router middleware
 app.use(router);
@@ -33,21 +33,20 @@ app.use(router);
 const db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 //connect mongoose to our db
-mongoose.connect(db, function(error){
+mongoose.connect(db, function (error) {
     //log errors connecting w/mongoose
     if (error) {
         console.log(error);
     }
     //or log a success message
-    else{
+    else {
         console.log("mongoose connection is successful");
     }
 });
 
-mongoose.Promise =global.Promise;
-
+mongoose.Promise = global.Promise;
 
 //listen on the port
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("Listening on port: " + PORT);
 });
