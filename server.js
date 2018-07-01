565ed0df7993328a271326563341e28bb32cdf2c
Addis Casco <addis.casco@utexas.edu>
2018-06-30 23:30:45 -0500

    trying something

--- server.js
+++ server.js
@@ -4,14 +4,13 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var axios = require("axios");
-var cheerio = require("cheerio");
 var rp = require('request-promise')
-
-var Article = require("./models/Article.js");
-
+var cheerio = require("cheerio");
 
 // Require models
-var db = require("./models");
+// var db = require("./models");
+var Article = require("./models/Article.js")
+//bypass the index.js and use the model directly
 
 //Set up port
 var PORT = process.env.PORT || 3000;
@@ -22,40 +21,42 @@ var app = express();
 // Logging requests
 app.use(logger("dev"));
 
-// Use body-parser 
+// Use body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Public folder as a static directory
 app.use(express.static("public"));
 mongoose.Promise = Promise;
 
-// Connect MONGODB or Localhost 
+// Connect MONGODB or Localhost
 // var MONGODB_URI = "mongodb://addthis:password410@ds123971.mlab.com:23971/heroku_w239b39v" || "mongodb://localhost/Scrape";
 
 //connect mongoose to our db
 
-//Connect to MongoDB
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/scraper" );
-var database = mongoose.connection;
-
-database.on( "error", function ( error ) {
-  console.log( "Mongoose Error: ", error );
-})
-
-database.once( "open", function () {
-  console.log( "Mongoose connected." );
-});
-
-// var database = "mongodb://addthis:password410@ds123971.mlab.com:23971/heroku_w239b39v" || "mongodb://localhost/scrape";
-
-// mongoose.connect(database, function (error) {
-//     if (error) {
+// var db = "mongodb://addthis:password410@ds123971.mlab.com:23971/heroku_w239b39v" || "mongodb://localhost/scrape";
+//
+// mongoose.connect(db, function (error){
+//     if(error){
 //         console.log(error);
 //     }
-//     else {
+//     else{
 //         console.log("connected");
 //     }
 // });
+
+//brandon
+//Connect to MongoDB db = scraper
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/scraper" );
+var db = mongoose.connection;
+
+db.on( "error", function ( error ) {
+  console.log( "Mongoose Error: ", error );
+} )
+
+db.once( "open", function () {
+  console.log( "Mongoose connected." );
+} );
+
 
 // mongoose.connect(MONGODB_URI, function (error) {
 //     if (error) {
@@ -68,25 +69,23 @@ database.once( "open", function () {
 
 // Routes
 app.get("/scrape", function (req, res) {
-    // Grabs body of HTML with request
+    // Grabs body of HTML with request-promise so I didn't have to refactor the code
     rp("http://www.nytimes.com/").then(function (response) {
         // Receive response into cheerio and save it to $ variable
         var $ = cheerio.load(response);
 
-        // Grabs h2 elements
-        $(".summary").each(function (i, el) {
+        $("article.story").each(function (i, el) {
 
             var outcome = {};
 
-            outcome.title = $(this).children('.story-heading').children('a').text();
-            console.log('title', outcome.title);
+            outcome.title = $(this).children('.story-heading').text();
+            // console.log('title', outcome.title);
+
+            outcome.link = $(this).children('.story-heading').children().attr('href')
+            // console.log('link', outcome.link);
 
             outcome.author = $(this).children('.byline').text();
-            append(outcome.author);
-
-
-            outcome.link = $(this).children(".story-heading").children().attr("href");
-            console.log('link', outcome.link);
+            // console.log('author', outcome.author);
 
             // outcome.title = $(this)
             //     .parent(".story theme-summary lede")
@@ -99,15 +98,16 @@ app.get("/scrape", function (req, res) {
             // outcome.link = $(this)
             //     .children("a")
             //     .attr("href");
+            //
+            // console.log(this)
+            // console.log("link: " + outcome.link);
 
-            // console.log("RESULT: " + outcome);
-
-            // // Creates a new Article 
+            // Creates a new Article
             if (outcome.link && outcome.title) {
 
-                db.Article.create(outcome)
+                Article.create(outcome)
                     .then(function (dbArticle) {
-                        console.log(dbArticle);
+                        console.log("db article create", dbArticle);
                     })
                     .catch(function (err) {
                         console.log(err);
@@ -120,7 +120,7 @@ app.get("/scrape", function (req, res) {
 
 //Gets all articles
 app.get("/articles", function (req, res) {
-    db.Article.find({})
+    Article.find({})
         .then(function (dbArticle) {
             res.json(dbArticle);
         })
